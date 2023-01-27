3c6f95de139b5240faf0ebbbe50a18ff342895a2
Majid Ali Khan <majidalykhan@gmail.com>
2023-01-27 18:31:43 +0500

    Scroll functionality

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,10 @@
 import axios from "axios";
-
 import React, { useState, useEffect } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import Movie from "./Movie";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState([]);
-
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -13,15 +12,38 @@ const Row = ({ title, fetchURL }) => {
     });
   }, [fetchURL]);
 
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <>
       <h2 className="text-white text-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          className="text-white cursor-pointer hidden z-10 group-hover:block"
+          size={40}
+          onClick={slideLeft}
+        />
+        <div
+          id={"slider" + rowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
             <Movie item={item} key={id} />
           ))}
         </div>
+        <MdChevronRight
+          className="text-white cursor-pointer hidden z-10 group-hover:block"
+          size={40}
+          onClick={slideRight}
+        />
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,11 @@ const Home = () => {
   return (
     <>
       <Main />
-      <Row title="Trending" fetchURL={requests.requestTrending} />
-      <Row title="Upcoming" fetchURL={requests.requestUpcoming} />
-      <Row title="Popular" fetchURL={requests.requestPopular} />
-      <Row title="Top Rated" fetchURL={requests.requestTopRated} />
-      <Row title="Action" fetchURL={requests.requestAction} />
+      <Row rowID="1" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowID="2" title="Upcoming" fetchURL={requests.requestUpcoming} />
+      <Row rowID="3" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowID="5" title="Action" fetchURL={requests.requestAction} />
     </>
   );
 };
